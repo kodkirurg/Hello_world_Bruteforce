2fc6d4e81e579c909a6b1ab1b24883d7d77f54fd
Johannes(raxxor) <johannes9533@hotmail.com>
2018-05-05 00:09:25 +0200

    workers added

--- go/hello_world_bruteforce.go
+++ go/hello_world_bruteforce.go
@@ -1,26 +1,43 @@
 package main
 
 import (
+	"log"
 	"os"
 	"os/exec"
 	"runtime"
+	"strconv"
+	"sync"
 	"time"
 )
 
 const helloWorld = "Hello World!"
-const timeBetweenFrameUpdate = 500 //Milliseconds
+const timeBetweenFrameUpdate = 5 //Milliseconds
+const workers int = 4
 
+var logger = log.New(os.Stdout, "", 0)
 var str string
+var mutex = &sync.Mutex{}
 
 func main() {
 	clearTerminal()
-	bruteForce()
+	var wg sync.WaitGroup
+	for x := 0; x < workers; x++ {
+		wg.Add(1)
+		go bruteForce(&wg, x+1)
+
+	}
+	wg.Wait() //wait for workers to join
+	time.Sleep(time.Second)
+	logger.Printf("\033[" + strconv.Itoa(workers+1) + ";0H")
+	logger.Printf("\n \n \n")
 }
 
-func bruteForce() {
+func bruteForce(wg *sync.WaitGroup, worker int) {
+	defer wg.Done() //when finished tell main thread
+
 	generatedSlice := generateCharSlice()
 	for _, v := range helloWorld {
-		index := binarySearch(generatedSlice, int(v))
+		index := binarySearch(generatedSlice, int(v), worker)
 		if index == -1 {
 			panic("Error; char missing")
 		}
@@ -28,27 +45,14 @@ func bruteForce() {
 	}
 }
 
-func printStatus(index int) {
-	time.Sleep(time.Millisecond * timeBetweenFrameUpdate)
-	clearTerminal()
-	println(str + string(generateCharSlice()[index]))
-	for i := range generateCharSlice() {
-		if i == index {
-			print(string(generateCharSlice()[index]))
-		} else {
-			print("_")
-		}
-	}
-}
-
 //returns -1 if not found and index if found
-func binarySearch(sortedSlice []int, element int) int {
+func binarySearch(sortedSlice []int, element int, workerLine int) int {
 	indexLower := 0
 	inBetweenIndex := len(sortedSlice) / 2
 	indexUpper := len(sortedSlice) - 1
 
 	for indexLower <= indexUpper {
-		printStatus(inBetweenIndex)
+		printWorker(inBetweenIndex, workerLine)
 		if sortedSlice[inBetweenIndex] > element { //go smaller
 			indexUpper = inBetweenIndex - 1
 		} else if sortedSlice[inBetweenIndex] < element { //go larger
@@ -59,6 +63,22 @@ func binarySearch(sortedSlice []int, element int) int {
 		inBetweenIndex = indexLower + (indexUpper-indexLower)/2
 	}
 	return -1
+}
+
+func printWorker(charIndex int, workerLine int) {
+	time.Sleep(time.Millisecond * timeBetweenFrameUpdate)
+	mutex.Lock()
+	var s string
+	for i := range generateCharSlice() {
+		if i == charIndex {
+			s = s + string(generateCharSlice()[charIndex])
+		} else {
+			s = s + "_"
+		}
+	}
+	logger.Printf("\033[" + strconv.Itoa(workerLine) + ";0H") //move to correct line for printing worker progress
+	logger.Printf(s + "\n")
+	mutex.Unlock()
 }
 
 func generateCharSlice() []int {
